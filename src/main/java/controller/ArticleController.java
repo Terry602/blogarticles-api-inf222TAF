6ea256c1802ddaa6191f75485e7469ee72c6af4a
Terry602 <terrynomo17@gmail.com>
2026-03-23 23:13:47 +0100

    Projet backend + interface + swagger

--- src/main/java/controller/ArticleController.java
+++ src/main/java/controller/ArticleController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/articles")
+@CrossOrigin(origins = "*")
 public class ArticleController {
 
     private final ArticleRepository repository;
